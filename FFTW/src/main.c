5f8c5ed8a20ac94924f29bf1438d449f5c9a8fb2
astrodiegog <digarza@ucsc.edu>
2026-03-11 02:39:47 -0700

    factor out creation of xarray and kxarrays

--- FFTW/src/main.c
+++ FFTW/src/main.c
@@ -25,8 +25,53 @@ double *create_xarr(int Nx, double xmin, double xmax)
 	return x_arr;
 }
 
+double *create_kxarr_c(int Nx, double dx)
+{   
+	int i;
+    int Nx_r = (int) ((Nx / 2.) + 1.); // number of fft bins for real fft
+
+	double *kx_arr_c = (double *) malloc(sizeof(double) * Nx);
+
+    /* Fill in positive frequencies */
+    for (i=0; i < Nx_r; i++)
+    {
+        kx_arr_c[i] = i / (Nx * dx);
+    }
+    /* Flip first negative frequency */
+    if (Nx % 2)
+    {
+        kx_arr_c[Nx_r] *= -1.;
+    }
+    else
+    {
+        kx_arr_c[Nx_r - 1] *= -1.;
+    }
+    /* Fill in negative frequencies */
+    for (i=Nx_r; i < Nx; i++)
+    {
+        kx_arr_c[i] = -1. * (Nx - i) / (Nx * dx);
+    }
+    
+    return kx_arr_c;
+}
 
 
+double *create_kxarr_r(int Nx, double dx)
+{   
+    int i;
+    int Nx_r = (int) ((Nx / 2.) + 1.); // number of fft bins for real fft
+    
+    double *kx_arr_r = (double *) malloc(sizeof(double) * Nx_r);
+
+    /* Fill in positive frequencies */
+    for (i=0; i < Nx_r; i++)
+    {
+        kx_arr_r[i] = i / (Nx * dx);
+    }
+
+	return kx_arr_r;
+
+}
 
 
 
@@ -44,63 +89,40 @@ int main(int argc, char **argv)
 		return 0;
 	}
 
+	/* Declare info for HDF5 file*/
 	char *FileName = "FFTWFun_out.h5";
 	hid_t file_id;
 	hid_t grp_1D_id;
 	hid_t grp_test_id;
 	hid_t dataspace_id_c, dataspace_id_r;
 
-	/* Create file */
-	file_id = H5Fcreate(FileName, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
-
+	/* Declare array of dimensions */
 	hsize_t dims_c[1], dims_r[1];
-	int Nx = 2048;
-	int Rank = 1; 
-	int i; // iterator
+	int Rank = 1;
 
-	/* Create x-arr */
+	/* Declare info for x-arr and kx arrays (for complex and real FFT) */
+	int Nx = 2048;
+    int Nx_r = (int) ((Nx / 2.) + 1.); // number of fft bins for real fft
 	double xmin = -5.;
 	double xmax = 5.;
     double dx = (xmax - xmin) / Nx;
-	double *x_arr;
+	double *x_arr, *kx_arr_c, *kx_arr_r;
 
+	/* Create file */
+    file_id = H5Fcreate(FileName, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+
+	/* Create x-arr and kx arrays (for complex and real FFT) */
 	x_arr = create_xarr(Nx, xmin, xmax);
+	kx_arr_c = create_kxarr_c(Nx, dx);
+    kx_arr_r = create_kxarr_r(Nx, dx);
 
-	/* Create kx-arr */
-	double kx_arr_c[Nx];
-	int Nx_r = (int) ((Nx / 2.) + 1.); // number of fft bins for real fft
-	double kx_arr_r[Nx_r];
-
-	/* Fill in positive frequencies */
-	for (i=0; i < Nx_r; i++)
-	{
-		kx_arr_r[i] = i / (Nx * dx);
-		kx_arr_c[i] = i / (Nx * dx);
-	}
-	/* Flip first negative frequency */
-	if (Nx % 2)
-	{
-		kx_arr_c[Nx_r] *= -1.;
-	}
-	else
-	{
-		kx_arr_c[Nx_r - 1] *= -1.;
-	}
-	/* Fill in negative frequencies */
-	for (i=Nx_r; i < Nx; i++)
-	{
-		kx_arr_c[i] = -1. * (Nx - i) / (Nx * dx);
-	}
-
-
-	/* Create dataspace */
+	/* Create dataspace for real and complex FFT */
 	dims_c[0] = Nx;
 	dims_r[0] = Nx_r;
 	dataspace_id_c = H5Screate_simple(Rank, dims_c, NULL);
     dataspace_id_r = H5Screate_simple(Rank, dims_r, NULL);
 
-
-	/* Create 1D group */
+	/* Create group for 1D tests */
 	grp_1D_id = H5Gcreate(file_id, "/OneDimensionalTests", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
 	/* Create+Write xarray & kxarrays attributes */
@@ -112,24 +134,27 @@ int main(int argc, char **argv)
 	status = H5Aclose(attr_id);
 
 	attr_id = H5Acreate(grp_1D_id, "kx_arr_c", H5T_IEEE_F64BE, dataspace_id_c, H5P_DEFAULT, H5P_DEFAULT);
-    status = H5Awrite(attr_id, H5T_NATIVE_DOUBLE, &kx_arr_c);
+    status = H5Awrite(attr_id, H5T_NATIVE_DOUBLE, kx_arr_c);
     status = H5Aclose(attr_id);
 
 	attr_id = H5Acreate(grp_1D_id, "kx_arr_r", H5T_IEEE_F64BE, dataspace_id_r, H5P_DEFAULT, H5P_DEFAULT);
-    status = H5Awrite(attr_id, H5T_NATIVE_DOUBLE, &kx_arr_r);
+    status = H5Awrite(attr_id, H5T_NATIVE_DOUBLE, kx_arr_r);
     status = H5Aclose(attr_id);
 
 	/* Run Test 1 */
-	hid_t dataset_id;
 	grp_test_id = H5Gcreate(grp_1D_id, "TestOne", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 	
 	double a = 2.;
-
 	RunTestOne(grp_test_id, &x_arr[0], dataspace_id_c, &kx_arr_c[0], dataspace_id_r, &kx_arr_r[0], Nx, Nx_r, a);
 
 
 
+	/* Free malloc-ed arrays*/
 	free(x_arr);
+	free(kx_arr_c);
+	free(kx_arr_r);
+
+	/* Close HDF5 objects */
 	status = H5Gclose(grp_test_id);
 	status = H5Gclose(grp_1D_id);
 	status = H5Fclose(file_id);
